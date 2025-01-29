3b046eab03a48c10d8cdc58962e1fd8505152218
Farhaan <gfarhanmohd7@gmail.com>
2025-01-29 19:14:59 +0530

    initial

---
+++ src/main/java/com/WeLink/videocall/VideocallApplication.java
@@ -0,0 +1,43 @@
+package com.WeLink.videocall;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.WeLink.videocall.user.User;
+import com.WeLink.videocall.user.UserService;
+
+@SpringBootApplication
+public class VideocallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideocallApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			UserService service
+	) {
+		return args -> {
+			service.register(User.builder()
+							.username("Ali")
+							.email("ali@mail.com")
+							.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("John")
+					.email("john@mail.com")
+					.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("Anny")
+					.email("anna@mail.com")
+					.password("aaa")
+					.build());
+		};
+	}
+
+}
